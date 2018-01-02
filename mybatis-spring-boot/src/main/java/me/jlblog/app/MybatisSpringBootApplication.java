5904ddbcebe19edb3002e3e638d81a5177e475d0
Giung Jo <ungss82@gmail.com>
2018-01-02 18:38:55 +0900

    고객관리 시스템 CRUD 완성 (but 한글 깨짐)

--- mybatis-spring-boot/src/main/java/me/jlblog/app/MybatisSpringBootApplication.java
+++ mybatis-spring-boot/src/main/java/me/jlblog/app/MybatisSpringBootApplication.java
@@ -4,7 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -14,5 +18,14 @@ public class MybatisSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MybatisSpringBootApplication.class, args);
+	}
+	
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	@Bean
+	CharacterEncodingFilter characterEncodingFilter(){
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		return filter;
 	}
 }
--- mybatis-spring-boot/src/main/java/me/jlblog/app/controller/CustomerRestController.java
+++ mybatis-spring-boot/src/main/java/me/jlblog/app/controller/CustomerRestController.java
@@ -1,21 +1,25 @@
 package me.jlblog.app.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import me.jlblog.app.domain.Customer;
 import me.jlblog.app.service.CustomerService;
 
 @RestController
-@RequestMapping("api/custommers")
+@RequestMapping("api/customers")
 public class CustomerRestController {
 	
 	@Autowired
@@ -38,11 +42,18 @@ public class CustomerRestController {
 	 * HTTP 메서드 POST 할당
 	 * API 정상 동작시 201 CREATED 반환
 	 * Customer 객체에 매핑을 위해 @RequestBody 애너테이션 설정
+	 * UriComponentsBuilder : 컨텍스트 상대로 URI를 쉽게 만들어 줌
 	 */
+	//@ResponseStatus(HttpStatus.CREATED)
 	@RequestMapping(method = RequestMethod.POST)
-	@ResponseStatus(HttpStatus.CREATED)
-	public Customer postCustomers(@RequestBody Customer customer){
-		return customerService.create(customer);
+	public ResponseEntity<Customer> postCustomers(@RequestBody Customer customer,
+			UriComponentsBuilder uriBuilder){
+		Customer created = customerService.create(customer);
+		URI location = uriBuilder.path("api/customers/{id}").buildAndExpand(created.getId()).toUri();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(location);
+		
+		return new ResponseEntity<>(created, headers, HttpStatus.CREATED);
 	}
 	
 	// 고객 한 명의 정보 업데이트
